095dae9124c3904d179c32b17f6fdf866dbc2ede
pyrothei <garett2777@gmail.com>
2023-05-30 07:21:31 -0700

    Added basic libraries and set up file

---
+++ catWatcher.py
@@ -0,0 +1,9 @@
+# This is for requesting the html
+import requests
+# This is for data scraping/organizing the html
+from bs4 import BeautifulSoup
+
+# Website to be used
+url = "https://www.oregonhumane.org/adopt/?type=cats"
+
+
